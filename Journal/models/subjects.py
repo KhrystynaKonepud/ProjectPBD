8d9fadbc29bd0f9d9553a9bf213e238e988432f5
Taisiia <taisiia_vanina@knu.ua>
2025-04-16 16:54:49 +0300

    Added file subjects.py

---
+++ Journal/models/subjects.py
@@ -0,0 +1,18 @@
+from mongoengine import Document, StringField, IntField, ListField, ReferenceField
+from .lecturer import Lecturer
+from .group import Group
+
+
+class Discipline(Document):
+
+    name = StringField(required=True, unique=True)
+    description = StringField()
+    lecturer = ReferenceField(Lecturer, required=True)
+    groups = ListField(ReferenceField(Group))
+    hours = IntField()
+    exam_type = StringField(choices=["Екзаменаційна", "Залікова", "Диференційований залік"])
+    class_types = ListField(StringField(choices=["Лекції", "Лабораторні", "Практичні"]))
+
+    def __str__(self):
+        return self.name
+
