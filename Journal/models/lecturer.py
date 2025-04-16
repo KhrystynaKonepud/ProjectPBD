5e1bd5cd4d2417033b97c031a1a57fc235a4c80a
AnastasiaSushkova <anastasia.sushkova@knu.ua>
2025-04-16 17:31:44 +0300

    міни у моделі lecturer

--- Journal/models/lecturer.py
+++ Journal/models/lecturer.py
@@ -6,7 +6,7 @@ class Lecturer(Document):
     email = EmailField(required=True, unique=True)
     password = StringField(required=True)
 
-    subjects = ListField(ReferenceField('Subjects'))  # або 'Subject', якщо ще не імпортований
+    subjects = ListField(ReferenceField('Subjects'))
     groups = ListField(ReferenceField('Groups'))
     created_journals = ListField(ReferenceField('Journal'))
 
