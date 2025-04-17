b2910c3da697fdcdfc43f017f404e607f52548d6
AnastasiaSushkova <anastasia.sushkova@knu.ua>
2025-04-17 19:48:55 +0300

    Додала роль до викладача

--- Journal/models/lecturer.py
+++ Journal/models/lecturer.py
@@ -6,6 +6,8 @@ class Lecturer(Document):
     email = EmailField(required=True, unique=True)
     password = StringField(required=True)
 
+    role = StringField(required=True, default="lecturer")
+
     subjects = ListField(ReferenceField('Subjects'))
     groups = ListField(ReferenceField('Groups'))
     created_journals = ListField(ReferenceField('Journal'))
